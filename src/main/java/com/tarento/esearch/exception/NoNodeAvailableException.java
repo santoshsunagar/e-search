7c3f2d890187bbc72c0686d60597ac5825d18dba
santhosh <santoshncet@gmail.com>
2017-06-29 17:33:54 +0530

    Added implementaiton for changes asked

---
+++ src/main/java/com/tarento/esearch/exception/NoNodeAvailableException.java
@@ -0,0 +1,14 @@
+package com.tarento.esearch.exception;
+
+public class NoNodeAvailableException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 4395495846828123203L;
+
+	public NoNodeAvailableException(String expMessagee) {
+		super(expMessagee);
+	}
+	
+}
